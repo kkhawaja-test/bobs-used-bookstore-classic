91a4b105553fe21232790cf9b9ccadf39b52f8a1
NoReply Amazon <no-reply@amazon.com>
2025-01-30 19:11:51 +0000

    [Commit] Iteration 1 #errors = 134 <= 137.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0246][4,12] The type or namespace name 'OwinStartupAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,18 +1,23 @@
-using Microsoft.AspNetCore.Owin;
-
-
-[assembly: OwinStartup(typeof(Bookstore.Web.Startup))]
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure services here
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             LoggingSetup.ConfigureLogging();
 
             ConfigurationSetup.ConfigureConfiguration();
 
+            // Update these methods to work with IApplicationBuilder instead of IAppBuilder
             DependencyInjectionSetup.ConfigureDependencyInjection(app);
 
             AuthenticationConfig.ConfigureAuthentication(app);
