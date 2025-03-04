ec05b328d12e7ed41b80438e569245e0f3004c54
NoReply Amazon <no-reply@amazon.com>
2025-03-04 21:29:50 +0000

    Revert "[Commit] Iteration 22 #errors = 11 <= 12."
    This reverts commit 08c5010c67a2c725115d7e742e8fdc211ec6962e.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,49 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            Configuration = configuration;
+            LoggingSetup.ConfigureLogging();
+
+            ConfigurationSetup.ConfigureConfiguration();
+
+            // Note: DependencyInjection setup should be moved to ConfigureServices method
+
+            // Note: Authentication config should be updated to use ASP.NET Core Identity
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            // Add other services as needed
+            // Move dependency injection setup here
+            // DependencyInjectionSetup.ConfigureDependencyInjection(services);
+
+            // Configure authentication here
             // services.AddAuthentication(...);
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                // Add other endpoint mappings as needed
-            });
         }
     }
 }
