c4617cd00e7a310285a8822710c2e9abab39fc5b
NoReply Amazon <no-reply@amazon.com>
2025-03-04 21:10:28 +0000

    Revert "[Commit] Iteration 3 #errors = 76 <= 78."
    This reverts commit 7212e64f59fe6df6e9a25006ad1427a66454b506.

--- app/Bookstore.Web/Areas/Admin/AdminAreaRegistration.cs
+++ app/Bookstore.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -1,20 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+
 
 namespace Bookstore.Web.Areas
 {
-    public class AdminAreaRegistration : IAreaRegistration
+    public class AdminAreaRegistration : AreaRegistration 
     {
-        public string AreaName => "Admin";
-
-        public void RegisterArea(IEndpointRouteBuilder endpoints)
+        public override string AreaName 
         {
-            endpoints.MapAreaControllerRoute(
-                name: "Admin_default",
-                areaName: "Admin",
-                pattern: "Admin/{controller=Home}/{action=Index}/{id?}",
-                defaults: new { area = "Admin" }
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }, namespaces: new[] { "Bookstore.Web.Areas.Admin.Controllers" }
             );
         }
     }
