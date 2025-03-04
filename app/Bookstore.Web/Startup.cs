b5935185d1a825dcca5edd801383e0ca1777e844
NoReply Amazon <no-reply@amazon.com>
2025-03-04 21:32:28 +0000

    [Commit] Iteration 23 #errors = 12 <= 12.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0103][11,13] The name 'LoggingSetup' does not exist in the current context``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            LoggingSetup.ConfigureLogging();
+// Configure logging using the ILoggerFactory
+            loggerFactory.AddConfiguration(app.ApplicationServices.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>().GetSection("Logging"));
 
+            // The rest of your Configure method remains the same
             ConfigurationSetup.ConfigureConfiguration();
 
             // Note: DependencyInjection setup should be moved to ConfigureServices method
@@ -19,6 +22,14 @@ namespace Bookstore.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add logging services
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(services.BuildServiceProvider().GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>().GetSection("Logging"))
+                       .AddConsole()
+                       .AddDebug();
+            });
+
             // Move dependency injection setup here
             // DependencyInjectionSetup.ConfigureDependencyInjection(services);
 
